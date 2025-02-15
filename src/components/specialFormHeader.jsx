b543c6b598d8a96c61c4ca9c2ef7b94dfa1b2c91
Sharon <omuwaste@gmail.com>
2025-02-15 01:24:55 +0100

    fix: form Header

--- src/components/specialFormHeader.jsx
+++ src/components/specialFormHeader.jsx
@@ -6,19 +6,30 @@ const SpecialFormHeader = ({ topic, stepValue }) => {
         <p className="text-base font-roboto">Step {stepValue}/3</p>
       </section>
 
-      <section className={`${stepValue === 2 ? "flex" : "hidden"} w-full`}>
-        <div className="w-[604px] sm:w-[232px] border-b-4 rounded-[5px] border-[var(--color-light-blue)]"></div>
-        <div className="w-[232px] sm:w-[604px] border-b-4 rounded-[5px] border-[var(--color-tertiary)]"></div>
-      </section>
+      <section className={"flex w-full"}>
+        {/* 2 */}
+        <div
+          className={`${
+            stepValue === 2 ? "flex" : "hidden"
+          } w-[1200px] sm:w-[700px] border-b-4 rounded-[5px] border-[var(--color-light-blue)]`}
+        ></div>
 
-      <section className={`${stepValue === 1 ? "flex" : "hidden"} w-full`}>
-        <div className="w-[604px] sm:w-[232px] border-b-4 rounded-[5px] border-[var(--color-light-blue)]"></div>
-        <div className="w-[232px] sm:w-[604px] border-b-4 rounded-[5px] border-[var(--color-tertiary)]"></div>
-      </section>
+        {/* 1 */}
+        <div
+          className={`${
+            stepValue === 1 ? "flex" : "hidden"
+          } w-[1200px] sm:w-[400px] border-b-4 rounded-[5px] border-[var(--color-light-blue)]`}
+        ></div>
 
-      <section className={`${stepValue === 3 ? "flex" : "hidden"} w-full`}>
-        <div className="w-[604px] sm:w-[232px] border-b-4 rounded-[5px] border-[var(--color-light-blue)]"></div>
-        <div className="w-[232px] sm:w-[604px] border-b-4 rounded-[5px] border-[var(--color-tertiary)]"></div>
+        {/* 3 */}
+        <div
+          className={`${
+            stepValue === 3 ? "flex" : "hidden"
+          } w-[1200px] sm:w-[150px] border-b-4 rounded-[5px] border-[var(--color-light-blue)]`}
+        ></div>
+
+        {/* same for all */}
+        <div className="w-full border-b-4 rounded-[5px] border-[var(--color-tertiary)]"></div>
       </section>
     </section>
   );
